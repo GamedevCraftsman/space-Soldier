3419e6ee05ae4e7f5e0362b8ae4fc47b06cb32a5
Danylo Solovko <ddssoolloovvkkoo4400@gmail.com>
2024-01-23 18:12:48 +0200

    Lesson 6.6

--- SpaceSolderPreview/Assets/_Game/Scripts/Shooting.cs
+++ SpaceSolderPreview/Assets/_Game/Scripts/Shooting.cs
@@ -1,13 +1,26 @@
 using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem.HID;
 public class Shooting : MonoBehaviour
 {
+    [SerializeField] int maxCountOfDecals;   
+    [SerializeField] float distance;
+    [SerializeField] float secondsToRemoveFire;   
+    [SerializeField] GameObject shootingFireParts;
+    [SerializeField] GameObject bulletHole;
     [SerializeField] GameObject flash;
     [SerializeField] Transform spawn;
-    [SerializeField] float distance;
 
+    List<GameObject> decals = new List<GameObject>();
+    List<GameObject> fires = new List<GameObject> ();
+
+    int countOfDecals = 0;
     const float totalDistance = 15;
     bool isPressed;
+    GameObject shootingFire;
+    GameObject decal;   
     void PlayerShootButton()
     {
         StartCoroutine(Flash());
@@ -15,13 +28,45 @@ public class Shooting : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, distance))
         {
-            distance = hit.distance;
+            Hit(hit);
         }
         else
         {
             distance = totalDistance;
         }
-        Debug.DrawRay(ray.origin, ray.direction * distance, Color.red, 1); // delete in future.
+    }
+
+    void Hit(RaycastHit hit)
+    {
+        distance = hit.distance;
+        if (IsDecalsAlot())
+        {
+            Destroy(decals[0]);
+            decals.RemoveAt(0);
+        }
+        StartCoroutine(SpawnBulletParts(hit));
+        decal = Decal(hit);
+        decals.Add(decal);
+        countOfDecals++;
+    }
+
+    bool IsDecalsAlot()
+    {
+        return countOfDecals > maxCountOfDecals;
+    }
+
+    IEnumerator SpawnBulletParts(RaycastHit hit)
+    {
+        shootingFire = Instantiate(shootingFireParts, hit.point + (hit.normal * 0.01f), Quaternion.identity);
+        fires.Add(shootingFire);
+        yield return new WaitForSeconds(secondsToRemoveFire);
+        Destroy(fires[0]);
+        fires.RemoveAt(0);
+    }
+
+    GameObject Decal(RaycastHit hit)
+    {
+        return Instantiate(bulletHole, hit.point + (hit.normal * 0.01f), Quaternion.FromToRotation(Vector3.up, hit.normal));
     }
 
     IEnumerator Flash()
